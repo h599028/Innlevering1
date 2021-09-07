e6aae0cef58d0fb2a9a7e34cea7f2d5ec439979b
Mathias Madsen Aaøyen <599028@stud.hvl.no>
2021-09-07 11:23:01 +0200

    Oppgave 3 lagt inn

--- Oblig1/src/no/hvl/dat100/OppgaveO3.java
+++ Oblig1/src/no/hvl/dat100/OppgaveO3.java
@@ -1,10 +1,18 @@
 package no.hvl.dat100;
 
+import javax.swing.JOptionPane;
+
 public class OppgaveO3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = Integer.parseInt(JOptionPane.showInputDialog("n!, skriv inn n"));
+		int sum = 1;
 
+		if (n > 0) {
+			for (; n > 0; n--)
+				sum = sum * n;
+			System.out.println(sum);
+		} else
+			System.out.println("Ugyldig verdi!");
 	}
-
 }
