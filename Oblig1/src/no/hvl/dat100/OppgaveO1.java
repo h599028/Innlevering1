2e55e3e54366bfa779892dfb9e8300790e3f0099
Mathias Madsen Aaøyen <599028@stud.hvl.no>
2021-09-07 11:08:53 +0200

    Oppgave 1 lagt inn

--- Oblig1/src/no/hvl/dat100/OppgaveO1.java
+++ Oblig1/src/no/hvl/dat100/OppgaveO1.java
@@ -1,10 +1,29 @@
 package no.hvl.dat100;
 
+import static java.lang.Double.parseDouble;
+import static javax.swing.JOptionPane.showInputDialog;
+
 public class OppgaveO1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double trinnskattProsent;
 
+		double inntekt = parseDouble(showInputDialog("Bruttoinntekt"));
+		
+		if (inntekt < 0)
+			System.out.println("Ugyldig inntektsmengde");
+		else {
+			if (inntekt > 1021550)
+				trinnskattProsent = 0.162;
+			else if (inntekt > 651250)
+				trinnskattProsent = 0.132;
+			else if (inntekt > 260100)
+				trinnskattProsent = 0.04;
+			else if (inntekt > 184800)
+				trinnskattProsent = 0.017;
+			else
+				trinnskattProsent = 0;
+			System.out.println("Trinnskatt: " +  inntekt * trinnskattProsent);
+		}
 	}
-
 }
